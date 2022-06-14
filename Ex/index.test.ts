ad2d2c06a8fd480b8cc93b5cb569fa11d6a45c54
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-14 16:24:34 -0300

    Finish Binary

---
+++ Ex/index.test.ts
@@ -0,0 +1,36 @@
+import { converterBinaryToDecimal } from ".";
+
+// Jest é um framework de teste em JavaScript projetado para garantir a correção de qualquer código JavaScript.
+
+// Vai criar um bloco que agrupa vários testes
+describe("Function that converts binary to decimal", () => {
+  // Vai executar um teste
+  // Primeiro argumento: É o nome do Teste
+  // Segundo argumento: É uma função que contém as expectativas para testar.
+
+  // expect().toBe() é a expectativa de ser alguma coisa.
+
+  test("Binary 1010 must be converted to 10 in decimal", () => {
+    const binaryString = "1";
+    const result = converterBinaryToDecimal(binaryString);
+    expect(result).toBe(1);
+  }),
+    test("Binary 10101010 must be converted to 170 in decimal", () => {
+      const binaryString = "10101010";
+      const result = converterBinaryToDecimal(binaryString);
+      expect(result).toBe(170);
+    }),
+    test("Binary 1110 must be converted to 14 in decimal", () => {
+      const binaryString = "1110";
+      const result = converterBinaryToDecimal(binaryString);
+      expect(result).toBe(14);
+    }), 
+    test("Should throws when it does not receive a binary number", () => {
+      const binaryString = "abc123";
+      expect(() => converterBinaryToDecimal(binaryString)).toThrow(
+        "Have to be system binary, that is, zero and one!"
+      );
+    });
+});
+
+// Para rodar nm test
--- Ex/index.ts
+++ Ex/index.ts
@@ -1,23 +1,24 @@
 // Função para converter o binário para
 // Vai receber um argumento chamado binary
-const converter = (binary) => {
+// Exportando a função para ser usada no Jest (Testes unitários)
+export function converterBinaryToDecimal(binary: string | number): number {
   // Regular Expressions (RegExp) que vai validar o padrão de caracteres do argumento da minha função
   // Ele vai verificar se a string possui caracteres que não são 0 ou 1
   const binarySystemCheck = new RegExp(/^[01]+$/g);
 
   // Alterando a tipagem do argumento de number para string
+  const binaryString = binary.toString();
+
   // Além de precisar inverter as string, então utilizei métodos em conjunto
-  // Sendo os três métodos:
-  // O split() vai dividir os elementos dentro de um Array, sendo a divsão a cada caracter
+  // Sendo os dois métodos:
+  // O split() vai dividir os elementos dentro de um Array, sendo a divisão a cada caracter
   // O reverse() vai inverter a ordem de um Array, por isso utilizei o split para criar um Array
-  // O join() vai unir todos os elementos de um Array em uma única string novamente
-  // O split cria um array pegando cada caracter e separando-os em String, o reverse inverte a ordem desse array e o join volta para uma string única sem array
-  const binaryString = binary.toString().split("").reverse();
+  const reversedBinaryArray = binaryString.split("").reverse();
 
   // O método test() executa uma busca por uma correspondência entre uma expressão regular e uma string.
   // Verificação para identificar se a string passada corresponde como binário
   // Deve corresponder com valores entre 0 e 1
-  if (!binarySystemCheck.test(binary))
+  if (!binarySystemCheck.test(binaryString))
     // Vai lançar um erro com uma descrição falando que precisa ser do sistema binário
     // Além de parar a execução da função
     throw new Error("Have to be system binary, that is, zero and one!");
@@ -34,15 +35,15 @@ const converter = (binary) => {
         - O Math.pow(base, expoente) tem o expoente que vai ser utilizado para elevar a base. No caso é o 2 elevado pela posição(i) do caracter
     Vai acumulando conforme passa por todos caracteres. Até finalmente retornar um valor final
    */
-  let storage = binaryString.reduce((acc, cur, idx) => {
-    return (acc += cur * Math.pow(2, idx));
+  let storage = reversedBinaryArray.reduce((acc, cur, idx) => {
+    return (acc += +cur * Math.pow(2, idx));
   }, 0);
 
   // Retorna o valor da váriavel decimal depois de ter feito todas as soma
   return storage;
-};
+}
 
-// Váriavel que armazena a função chamada com um argumento
-// O argumento deve ser um número binário que será convertido
-const call = converter(100);
-console.log(call);
+// Chamando a função
+converterBinaryToDecimal(1);
+
+// Para rodar npm run start-index
